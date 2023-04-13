39f3fb555ac27dd8fa21e935d7474750919b6a67
SaimSaeed <79637366+SaimSaeed@users.noreply.github.com>
2023-04-14 00:31:29 +0500

    Initial commit

---
+++ src/components/TextForm.js
@@ -0,0 +1,64 @@
+import React,{useState} from "react";
+
+export default function TextForm(props){
+  
+    const handleUpClick = ()=>{
+        let newText = text.toUpperCase();
+        setText(newText);
+        props.showAlert("Converted to UpperCase","success");
+    }
+    const handleLoClick = ()=>{
+        let newText = text.toLowerCase();
+        setText(newText);
+        props.showAlert("Converted to LowerCase","success");
+    }
+    const handleClearClick = ()=>{
+        let newText = "";
+        setText(newText);
+        props.showAlert("Text has been cleared","success");
+    }
+    const handleonChange= (event)=>{
+        console.log("on Change")
+setText(event.target.value);
+    }
+    const handleCopy=()=>{
+       var text = document.getElementById("myBox");
+       text.select();
+       navigator.clipboard.writeText(text.value);
+       props.showAlert("Text is copied","success");
+    }
+    const handleExtraSpaces= ()=>{
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+    }
+    const [text,setText] = useState("");
+    return(
+<>
+<div className="container">
+<h2  style={{color:props.mode==="light"? "#042743":"white"}}>{props.heading}</h2>
+<div className="mb-3">
+  <textarea className="form-control" value={text}  style={{backgroundColor:props.mode==="light"? "white":"grey",color:props.mode==="light"? "#042743":"white"}}  onChange={handleonChange} id="myBox" rows="8"></textarea>
+</div>
+<button className="btn btn-primary mx-1" onClick={handleUpClick}>Covert to Uppercase</button>
+<button className="btn btn-primary mx-1" onClick={handleLoClick}>Covert to Lowercase</button>
+<button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
+<button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
+<button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+</div>
+<div className="container my-3" style={{color:props.mode==="light"? "#042743":"white"}}>
+<h2>Your Text Summary</h2>
+<p>{text.split(" ").length} words and {text.length} characters</p>
+<p>{0.008* text.split(" ").length} Minutes read</p>
+<h2>Preview</h2>
+<p>{text.length>0? text:"Enter Your Text to Preview it Here"}</p>
+
+
+
+
+</div>
+
+
+
+</>
+    );
+}
