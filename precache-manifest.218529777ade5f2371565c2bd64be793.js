f701415cf45ba936871bd13fc7460bea102507c0
SaimSaeed <79637366+SaimSaeed@users.noreply.github.com>
2023-04-14 22:13:40 +0500

    Updates

---
+++ precache-manifest.218529777ade5f2371565c2bd64be793.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4af1ec133d6a2cff57f6",
+    "url": "/textutils/static/css/main.2a8a1bbe.chunk.css"
+  },
+  {
+    "revision": "4af1ec133d6a2cff57f6",
+    "url": "/textutils/static/js/main.4af1ec13.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/textutils/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/textutils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "91449b700f0ab8a2a413",
+    "url": "/textutils/static/js/runtime~main.91449b70.js"
+  },
+  {
+    "revision": "54cca033e34e300231929d3474c9ab57",
+    "url": "/textutils/index.html"
+  }
+];
